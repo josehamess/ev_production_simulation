f2c5595a8b5d0fc4ac83402b8b98fe8e3cb5b874
Joseph Amess <72140426+josehamess@users.noreply.github.com>
2024-03-14 12:28:12 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -203,9 +203,9 @@ def plot_data(df, df_proj):
         ax.plot(df_proj['Date'], correct_first_point(df, df_proj, name), color=colours[i], linestyle='--')
         ax.plot(df[df['name'] == name + ' total']['Date'].astype(int), df[df['name'] == name + ' total']['Vehicle Count'] / 1000000, label=labels[i], color=colours[i])
         #plt.scatter(x_points_dict[name], [y / 1000000 for y in y_points_dict[name]], color=colours[i], s=20)
-    tick_positions = np.arange(int(df['Date'].iloc[0]), df_proj['Date'].iloc[-1], 1)  # Adjust the range and step as needed
-    ax.set_xticks(tick_positions)
-    ax.set_xticklabels([str(x) for x in range(min(list(df[df['name'] == name + ' total']['Date'].astype(int))), max(df_proj['Date']) + 1, 1)], rotation='vertical')
+    #tick_positions = np.arange(int(df['Date'].iloc[0]), df_proj['Date'].iloc[-1], 1)  # Adjust the range and step as needed
+    #ax.set_xticks(tick_positions)
+    #ax.set_xticklabels([str(x) for x in range(min(list(df[df['name'] == name + ' total']['Date'].astype(int))), max(df_proj['Date']) + 1, 1)], rotation='vertical')
     ax.set_xlabel('date')
     ax.set_ylabel('total vehicles (millions)')
     ax.legend()
